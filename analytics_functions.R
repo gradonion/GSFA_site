2fb0edd444f61701f4cce6b824f92ea05af03cf2
gradonion <zhouyf@uchicago.edu>
2020-11-05 14:54:24 -0600

    first commit

---
+++ analytics_functions.R
@@ -0,0 +1,221 @@
+ZZT_mean_diff <- function(A_mat, B_mat){
+  if (nrow(A_mat) != nrow(B_mat)){
+    stop("Please make sure the two matrices have the same number of rows (samples).")
+  }
+  zzt_diff <- A_mat %*% t(A_mat) - B_mat %*% t(B_mat)
+  return(sum(abs(zzt_diff)) / nrow(zzt_diff)^2)
+}
+
+factor_matrix_regression <- function(factors, G_mat){
+  # factors: nxk, G_mat: nxm
+  pval_tb <- matrix(nrow = ncol(factors), ncol = ncol(G_mat))
+  beta_reg_tb <- pval_tb
+  if (is.null(colnames(G_mat))){
+    colnames(G_mat) <- 1:ncol(G_mat)
+  }
+  colnames(pval_tb) <- paste0("pval-", colnames(G_mat))
+  colnames(beta_reg_tb) <- paste0("beta_reg-", colnames(G_mat))
+  for (i in 1:ncol(factors)){
+    for (j in 1:ncol(G_mat)){
+      regress_mat <- data.frame(G = G_mat[, j], Z = factors[, i])
+      lm.summary <- summary(lm(Z ~ G, data = regress_mat))
+      pval_tb[i, j] <- lm.summary$coefficients[2, 4]
+      beta_reg_tb[i, j] <- lm.summary$coefficients[2, 1]
+    }
+  }
+  return(list(pval = pval_tb, beta = beta_reg_tb))
+}
+
+make_gibbs_res_tb <- function(gibbs_obj, G){
+  K <- ncol(gibbs_obj$Z.pm)
+  res_tb <- data.frame(index = 1:K, pi = gibbs_obj$pi_pm[, 1])
+  sum_var <- rep(0, K)
+  for (i in 1:K) {
+    mat_tmp <- outer(gibbs_obj$Z.pm[, i], gibbs_obj$W.pm[, i])
+    sum_var[i] <- sum(apply(mat_tmp, 2, var))
+  }
+  res_tb$sum_var <- sum_var
+  
+  if (is.null(ncol(G))){
+    if (length(G) != nrow(gibbs_obj$Z.pm)){
+      stop("Number of samples in genotype and in the gibbs object do not match!")
+    }
+    res_tb$beta_pm <- gibbs_obj$beta_pm[, 1]
+    res_tb$beta_reg <- rep(NA, K)
+    res_tb$pval <- rep(NA, K)
+    for (i in 1:K){
+      lm.summary <- summary(lm(gibbs_obj$Z.pm[, i] ~ G))
+      res_tb$beta_reg[i] <- lm.summary$coefficients[2, 1]
+      res_tb$pval[i] <- lm.summary$coefficients[2, 4]
+    }
+  } else {
+    if (nrow(G) != nrow(gibbs_obj$Z.pm)){
+      stop("Number of samples in genotype and in the gibbs object do not match!")
+    }
+    res_regress <- factor_matrix_regression(gibbs_obj$Z.pm, G)
+    tmp_beta_pm <- t(gibbs_obj$beta_pm)
+    colnames(tmp_beta_pm) <- paste0("beta_pm-", 1:ncol(tmp_beta_pm))
+    res_tb <- cbind(res_tb, tmp_beta_pm,
+                    res_regress$beta, res_regress$pval)
+  }
+  return(res_tb)
+}
+
+make_flash_res_tb <- function(flashier_obj, G){
+  K <- flashier_obj$n.factors
+  flash_res_tb <- data.frame(matrix(nrow = K, ncol = 3))
+  names(flash_res_tb) <- c('pve', 'pi1', 'lfsr_0.05')
+  flash_res_tb$pve <- flashier_obj$pve
+  for (i in 1:K){
+    flash_res_tb$pi1[i] <- flashier_obj$fitted.g[[2]][[i]]$pi[2]
+    factor_lfsr <- flashier_obj$loadings.lfsr[[2]][, i]
+    flash_res_tb$lfsr_0.05[i] <- sum(factor_lfsr < 0.05) / length(factor_lfsr)
+  }
+  if (is.null(ncol(G))){
+    flash_res_tb$beta_reg <- rep(NA, K)
+    flash_res_tb$pval <- rep(NA, K)
+    for (i in 1:K){
+      flash.summary <- summary(lm(flashier_obj$loadings.pm[[1]][, i] ~ G))
+      flash_res_tb$beta_reg[i] <- flash.summary$coefficients[2, 1]
+      flash_res_tb$pval[i] <- flash.summary$coefficients[2, 4]
+    }
+  } else {
+    res_regress <- factor_matrix_regression(flashier_obj$loadings.pm[[1]], G)
+    flash_res_tb <- cbind(flash_res_tb, res_regress$beta, res_regress$pval)
+  }
+  return(flash_res_tb)
+}
+
+plot_pval_heatmap <- function(heatmap_matrix, factor_annot = NULL, snp_annot = NULL){
+  # 'heatmap_matrix' has factors in the rows and SNPs in the columns
+  col_fun <- circlize::colorRamp2(c(0, 3, 15), c("blue", "white", "red"))
+  main_lgd <- Legend(col_fun = col_fun, title = "-log10(Factor~KO p value)", at = seq(0, 15, 3))
+  lgd_list <- list(main = main_lgd)
+  
+  if (is.null(snp_annot)){
+    column_annot <- NULL
+    column_lgd <- NULL
+  } else {
+    snp_col_fun <- circlize::colorRamp2(c(-0.46, 0, 0.46), c("turquoise2", "white", "orange2"))
+    column_annot <- columnAnnotation(beta = snp_annot,
+                                     col = list(beta = snp_col_fun),
+                                     show_annotation_name = T,
+                                     annotation_name_side = "left",
+                                     show_legend = F)
+    column_lgd <- Legend(col_fun = snp_col_fun, title = "PLIER~SNP beta", at = seq(-0.4, 0.4, 0.2))
+    lgd_list[["column"]] <- column_lgd
+  }
+  
+  if (is.null(factor_annot)){
+    row_annot <- NULL
+    row_lgd <- NULL
+  } else {
+    factor_col_fun <- circlize::colorRamp2(c(0, 0.7, 1), c("gold", "lightgreen", "seagreen"))
+    row_annot <- rowAnnotation(pi1 = factor_annot,
+                               col = list(pi1 = factor_col_fun),
+                               show_annotation_name = T,
+                               show_legend = F)
+    row_lgd <- Legend(col_fun = factor_col_fun, title = "Density (pi1)", at = seq(0, 1, 0.2))
+    lgd_list[["row"]] <- row_lgd
+  }
+  
+  # pd <- packLegend(main_lgd, column_lgd, row_lgd, direction = "vertical")
+  ht <- Heatmap(-log10(heatmap_matrix), col = col_fun, name = "-log10(p value)",
+                row_title = "Factors",  column_title = "KO Perturbations",
+                cluster_rows = F, cluster_columns = F,
+                show_heatmap_legend = F,
+                top_annotation = column_annot) + row_annot
+  draw(ht, annotation_legend_list = lgd_list)
+}
+
+source("/project2/xinhe/yifan/GTEx/scripts/qqplot_uniform.R")
+summ_pvalues <- function(pvalues, title_text = NULL){
+  requireNamespace("gridExtra", quietly = TRUE)
+  # distribution histogram
+  plot1 <- histogram(pvalues, col = 'grey', type = "count",
+                     xlim = c(0, 1), breaks = 50, main = "p value distribution")
+  # uniform qq-plot
+  plot2 <- qqunif.plot(pvalues, main = "p value qq-plot")
+  gridExtra::grid.arrange(plot1, plot2, ncol = 2, top = title_text)
+}
+
+plot_pairwise.corr_heatmap <- function(input_mat, corr_type = "pearson",
+                                       name = NULL, return_corr = FALSE){
+  # Please store samples in the columns of 'input_mat'.
+  corr_mat <- diag(ncol(input_mat))
+  for (i in 1:(ncol(input_mat)-1)){
+    for (j in (i+1):ncol(input_mat)){
+      if (corr_type == "pearson"){
+        corr_mat[i, j] <- cor(input_mat[, i], input_mat[, j], method = "pearson")
+      } else if (corr_type == "jaccard"){
+        corr_mat[i, j] <- sum(input_mat[, i] * input_mat[, j]) /
+          sum(input_mat[, i] + input_mat[, j] > 0)
+      } else {
+        stop("Please provide a valid method to compute pairwise correlation.")
+      }
+      corr_mat[j, i] <- corr_mat[i, j]
+    }
+  }
+  if (is.null(colnames(input_mat))){
+    rownames(corr_mat) <- 1:ncol(input_mat)
+    colnames(corr_mat) <- 1:ncol(input_mat)
+  } else {
+    rownames(corr_mat) <- colnames(input_mat)
+    colnames(corr_mat) <- colnames(input_mat)
+  }
+  
+  if (corr_type == "pearson"){
+    ht <- Heatmap(corr_mat,
+                  col = circlize::colorRamp2(c(-1, 0, 1), c("blue", "white", "red")),
+                  name = "Pearson Correlation",
+                  row_title = name, column_title = name,
+                  cluster_rows = F, cluster_columns = F,
+                  row_names_gp = gpar(fontsize = 8), column_names_gp = gpar(fontsize = 8))
+  }
+  if (corr_type == "jaccard"){
+    ht <- Heatmap(corr_mat,
+                  col = circlize::colorRamp2(c(0, 0.5, 1), c("white", "steelblue1", "steelblue4")),
+                  name = "Jaccard Correlation",
+                  row_title = name, column_title = name,
+                  cluster_rows = F, cluster_columns = F,
+                  row_names_gp = gpar(fontsize = 8), column_names_gp = gpar(fontsize = 8))
+  }
+  draw(ht)
+  if (return_corr){
+    return(corr_mat)
+  }
+}
+
+plot_PIP_hist_grid <- function(PIP_mat, cutoff = 0.5){
+  plot.lst <- list()
+  for (i in 1:ncol(PIP_mat)){
+    pi1 <- mean(PIP_mat[, i] > cutoff)
+    plot_df <- data.frame(prob = PIP_mat[, i])
+    p <- ggplot(plot_df, aes_string("prob")) +
+      geom_histogram(bins = 50) +
+      geom_vline(xintercept = cutoff, color = "red") +
+      labs(title = paste0("Factor ", i, ": ", signif(pi1, digits = 2))) +
+      theme(axis.title = element_blank())
+    plot.lst[[i]] <- p
+  }
+  args <- c(plot.lst, list(nrow = 4,
+                           left = "Count", bottom = "PIP"))
+  do.call(grid.arrange, args)
+}
+
+print_enrich_tb <- function(enrich_list, qvalue_cutoff = 0.05){
+  for (i in 1:length(enrich_list)){
+    signif_tb <- enrich_list[[i]] %>% filter(qvalue < qvalue_cutoff) %>%
+      select(ID, Description, GeneRatio, BgRatio, pvalue, qvalue)
+    # signif_tb$pvalue <- format.pval(signif_tb$pvalue, digits = 3)
+    # signif_tb$qvalue <- format.pval(signif_tb$qvalue, digits = 3)
+    signif_tb$pvalue <- format(signif_tb$pvalue, digits = 3)
+    signif_tb$qvalue <- format(signif_tb$qvalue, digits = 3)
+    if (nrow(signif_tb) > 0){
+      print(kable(signif_tb,
+                  caption = paste("Factor", i, ":", nrow(signif_tb), "significant GO terms")) %>%
+              kable_styling() %>%
+              scroll_box(width = '100%', height = '400px'))
+    }
+  }
+}
