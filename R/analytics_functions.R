e0773fa29cb4710e2706a4de58cabde4df175830
gradonion <zhouyf@uchicago.edu>
2021-11-02 18:15:30 -0500

    LUHMES

--- R/analytics_functions.R
+++ R/analytics_functions.R
@@ -149,7 +149,10 @@ dotplot_beta_PIP <- function(beta_pip_matrix, beta_pm_matrix,
 dotplot_effectsize <- function(effect_matrix, lfsr_matrix,
                                reorder_markers = colnames(effect_matrix),
                                color_lgd_title = "Estimated effect size",
-                               size_lgd_title = "LFSR"){
+                               size_lgd_title = "LFSR",
+                               max_score = 0.2,
+                               min_score = -0.2,
+                               by_score = 0.1){
   # Both inputs should be gene by guide/marker matrices,
   # Dots will be colored by effect size and sized by 1-LFSR value.
   lfsr_binning <- function(lfsr){
@@ -179,8 +182,8 @@ dotplot_effectsize <- function(effect_matrix, lfsr_matrix,
     mutate(LFSR = pip_plot_df$LFSR,
            gene = factor(gene, levels = rownames(effect_matrix)),
            Perturbation = factor(Perturbation, levels = reorder_markers))
-  combined_plot_df$Effect_size[combined_plot_df$Effect_size> 0.2] <- 0.2
-  combined_plot_df$Effect_size[combined_plot_df$Effect_size< -0.2] <- -0.2
+  combined_plot_df$Effect_size[combined_plot_df$Effect_size> max_score] <- max_score
+  combined_plot_df$Effect_size[combined_plot_df$Effect_size< min_score] <- min_score
   combined_plot_df <- combined_plot_df %>%
     rowwise() %>%
     mutate(LFSR_bin = lfsr_binning(LFSR)) %>%
@@ -188,9 +191,9 @@ dotplot_effectsize <- function(effect_matrix, lfsr_matrix,
   plot_out <- ggplot(combined_plot_df) +
     geom_point(aes(x = Perturbation, y = gene,
                    size = LFSR_bin, color = Effect_size)) +
-    scale_color_gradientn(limits = c(-0.2, 0.2),
+    scale_color_gradientn(limits = c(min_score, max_score),
                           colours = c("blue3", "blue", "grey90", "red", "red3"),
-                          breaks = seq(-0.1, 0.1, 0.1)) +
+                          breaks = seq(min_score, max_score, by_score)) +
     theme_void() +
     theme(axis.text.x = element_text(size = 13, angle = 90, hjust = 1),
           axis.text.y = element_text(size = 13),
